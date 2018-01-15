dbba241945a774dde50b8edc9869ec863fb20a08
malcolmccarlson <malcolm.c.carlson@gmail.com>
2018-01-14 19:49:12 -0600

    converted program to a cli using argparse

--- All_In_BloomSky.py
+++ All_In_BloomSky.py
@@ -2,7 +2,7 @@ import bloomsky_api
 import requests
 import json
 from configparser import ConfigParser
-
+import argparse
 
 Config = ConfigParser()
 Config.read('config.ini')
@@ -12,6 +12,13 @@ myurl = Config.get('Authentication','DEFAULT_API_URL')
 
 client = bloomsky_api.BloomSkyAPIClient(api_key=myapikey)
 mydata = client.get_data()
+
+
+parser = argparse.ArgumentParser(description='Access BloomSky weather data')
+parser.add_argument('-s', '--showkeys', action='store_true')
+parser.add_argument('-k', '--key', metavar='', required=True, help='Passes key in and gets result.')
+args = parser.parse_args()
+
 
 def getbsdata():
     r = requests.get(myurl, headers={'Authorization': myapikey})
@@ -33,19 +40,27 @@ def getkeys():
     for items in mydata:
         for key,value in recursive_items(items):
             keylist.append(key)
-        print (keylist)
+        return (keylist)
 
 def getdata(mykey):
     mydata = getbsdata()
+    keyval = {}
     for items in mydata:  #unpack the list of dictionaries
         for key, value in recursive_items(items): #search through the key value pares of the nested dicts for specific key
             if key == mykey:
-                return (key,value)
+                keyval = (key,value)
+                return keyval
+
 
 
 
 if __name__ == '__main__':
-    #getkeys()
-    mytemp = getdata('Temperature')
-    print (mytemp)
+    bsinfo = getdata(args.key)
+    if args.showkeys:
+        print (args.showkeys)
+    elif args.key:
+        print (bsinfo)
+    else:
+        pass
+
 
